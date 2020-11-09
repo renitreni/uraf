215add0048ca933a6a576011a49ed343105a3eca
Renier Trenuela <rtrenuela@objectbright.com>
2020-11-09 09:43:59 +0800

    binding fix

--- resources/views/email/complain.blade.php
+++ resources/views/email/complain.blade.php
@@ -3,42 +3,42 @@
         <h3>General Information</h3>
         <hr>
     </div>
-    <div class="form-group col-md-auto"><strong>First Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>@{{ full_details.first_name }}</em></div>
-    <div class="form-group col-md-auto"><strong>Middle Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>@{{ full_details.middle_name }}</em></div>
-    <div class="form-group col-md-auto"><strong>Last Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<em>@{{ full_details.last_name }}</em></div>
-    <div class="form-group col-md-auto"><strong>Gender</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>@{{ full_details.gender }}</em></div>
-    <div class="form-group col-md-auto"><strong>Passport Number</strong>&nbsp;&nbsp;<em>@{{ full_details.passport }}</em></div>
-    <div class="form-group col-md-auto"><strong>IQAMA</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<em>@{{ full_details.iqama }}</em></div>
+    <div class="form-group col-md-auto"><strong>First Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>{{ $full_details.first_name }}</em></div>
+    <div class="form-group col-md-auto"><strong>Middle Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>{{ $full_details.middle_name }}</em></div>
+    <div class="form-group col-md-auto"><strong>Last Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<em>{{ $full_details.last_name }}</em></div>
+    <div class="form-group col-md-auto"><strong>Gender</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>{{ $full_details.gender }}</em></div>
+    <div class="form-group col-md-auto"><strong>Passport Number</strong>&nbsp;&nbsp;<em>{{ $full_details.passport }}</em></div>
+    <div class="form-group col-md-auto"><strong>IQAMA</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<em>{{ $full_details.iqama }}</em></div>
     <div class="form-group col-md-12 mt-2">
         <h3>Occupation</h3>
         <hr>
     </div>
-    <div class="form-group col-md-auto"><strong>Current Position</strong>&nbsp; &nbsp;&nbsp;<em>@{{ full_details.occupation }}</em></div>
+    <div class="form-group col-md-auto"><strong>Current Position</strong>&nbsp; &nbsp;&nbsp;<em>{{ $full_details.occupation }}</em></div>
     <div class="form-group col-md-12 mt-2">
         <h3>Contact Information</h3>
         <hr>
     </div>
-    <div class="form-group col-md-auto"><strong>E-mail or Facebook Name</strong>&nbsp;&nbsp;<em>@{{ full_details.email_address }}</em></div>
-    <div class="form-group col-md-auto"><strong>Primary Contact Number</strong>&nbsp;&nbsp;<em>@{{ full_details.contact_number }}</em></div>
-    <div class="form-group col-md-auto"><strong>Other Contact Number</strong>&nbsp; &nbsp; &nbsp;&nbsp;<em>@{{ full_details.contact_number2 }}</em></div>
-    <div class="form-group col-md-auto"><strong>Location KSA</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>@{{ full_details.location_ksa }}</em></div>
+    <div class="form-group col-md-auto"><strong>E-mail or Facebook Name</strong>&nbsp;&nbsp;<em>{{ $full_details.email_address }}</em></div>
+    <div class="form-group col-md-auto"><strong>Primary Contact Number</strong>&nbsp;&nbsp;<em>{{ $full_details.contact_number }}</em></div>
+    <div class="form-group col-md-auto"><strong>Other Contact Number</strong>&nbsp; &nbsp; &nbsp;&nbsp;<em>{{ $full_details.contact_number2 }}</em></div>
+    <div class="form-group col-md-auto"><strong>Location KSA</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>{{ $full_details.location_ksa }}</em></div>
     <div class="form-group col-md-12 mt-2">
         <h3>Employer Information</h3>
         <hr>
     </div>
-    <div class="form-group col-md-auto"><strong>Employer Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>@{{ full_details.employer_name }}</em></div>
-    <div class="form-group col-md-auto"><strong>Employer Contact Number</strong>&nbsp;&nbsp;<em>@{{ full_details.employer_contact }}</em></div>
+    <div class="form-group col-md-auto"><strong>Employer Name</strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<em>{{ $full_details.employer_name }}</em></div>
+    <div class="form-group col-md-auto"><strong>Employer Contact Number</strong>&nbsp;&nbsp;<em>{{ $full_details.employer_contact }}</em></div>
     <div class="form-group col-md-auto"><strong>Recruitment Agency in Saudi</strong> </div>
     <div class="form-group col-md-auto"><strong>Recruitment Agency in Philippines</strong> </div>
     <div class="form-group col-md-12 mt-2">
         <h3>Complaint</h3>
         <hr>
     </div>
-    <div class="form-group col-md-12 mt-2"><strong>Concerns of the sender</strong><br><em>@{{ full_details.complain }}</em></div>
+    <div class="form-group col-md-12 mt-2"><strong>Concerns of the sender</strong><br><em>{{ $full_details.complain }}</em></div>
     <div class="form-group col-md-12 mt-2">
         <h3>Sender&apos;s Coordinates</h3>
         <hr>
     </div>
-    <div class="form-group col-md-auto"><strong>Latitude</strong>&nbsp; &nbsp; &nbsp; &nbsp;<em>@{{ full_details.actual_langitude }}</em></div>
-    <div class="form-group col-md-auto"><strong>Longtitude</strong>&nbsp; <em>@{{ full_details.actual_longitude }}</em></div>
+    <div class="form-group col-md-auto"><strong>Latitude</strong>&nbsp; &nbsp; &nbsp; &nbsp;<em>{{ $full_details.actual_langitude }}</em></div>
+    <div class="form-group col-md-auto"><strong>Longtitude</strong>&nbsp; <em>{{ $full_details.actual_longitude }}</em></div>
 </form>
